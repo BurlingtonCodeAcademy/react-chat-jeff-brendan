2fb27db420d448c0e38904fa851ea522d2acfd75
Jeff Van Etten <jvanetten19@gmail.com>
2020-10-28 16:31:18 -0400

    Database is linked

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
 const port = process.env.PORT || 8000;
 const app = express();
 const staticDir = process.env.DEV ? "./client/public" : "./client/build";
+
+const DataStore = require("./data.js");
+let myDb = new DataStore(`mongodb+srv://BCAadmin:${process.env.DBPASS}@cluster0.npdxy.mongodb.net/test?retryWrites=true&w=majority`, "library", "inventory");
 
 app.use(express.static(staticDir));
 
