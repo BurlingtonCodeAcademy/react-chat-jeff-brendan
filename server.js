2950f7b9be1a0de59e55ecb10a9d6b57900e787a
Jeff Van Etten <jvanetten19@gmail.com>
2020-10-29 17:53:56 -0400

    new updates

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
 //middleware
 app.use(express.urlencoded({extended: true }));
-//app.use(express.static("./src"))    <---- do we need this?
+//app.use(express.static("./public"))    <---- do we need this?
 app.use(express.static(staticDir));
 
 //Database integration 
@@ -18,13 +18,22 @@ let myDb = new MessageStore(`mongodb+srv://BCAadmin:${process.env.DBPASS}@cluste
 
 
 //set up an API endpoint that reads from database, and sends JSONs to endpoint
-
+app.get("/allrecords", async (req, res) => {
+  let results = await myDb.run()
+  res.send(results)
+})
 
 //show all records
-
+app.get("/allrecords", async (req, res) => {
+  res.sendFile(path.resolve("./public/placeholder.html"))
+})
 
 
 //need api endpoint for serving a single file from homepage main chat
+app.get('/placeholder/:id', (req, res) => {
+  let record = await myDb.showOne(req.params.id)
+  res.send(record)
+})
 
 //require an api endpoint for serving all the files in side chat
 
